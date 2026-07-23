6e2a04a084c137b61bd433a523c12b8aeca611a9
Serg Medvedev <medvserg@mail.ru>
2026-07-23 19:54:43 +0300

    refactor: clean up text_utils.py, move tests to main.py, add type hints

--- src/text_utils.py
+++ src/text_utils.py
@@ -1,11 +1,3 @@
 ﻿# Функция, которая принимает текст и возвращает его развернутым
-def reverse_text(text):
+def reverse_text(text: str) -> str:
     return text[::-1]
-
-# Проверяем работу функции
-original = "Привет, Skypro!"
-result = reverse_text(original)
-
-print("Оригинал:", original)
-print("Реверс:", result)
-
